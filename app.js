7d3875e73b3f11711e2f90fd7c77b972b057e823
Anju Kumari <anjukumari7ms@gmail.com>
2023-07-09 11:30:04 -0700

    app modified

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
+mongoose.connect(process.env.MONGO_URI);
 const app = require('express')();
 const http = require('http').Server(app);
 const userRoute = require('./routes/userRoute');
